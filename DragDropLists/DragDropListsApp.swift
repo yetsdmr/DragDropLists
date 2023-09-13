c63866c17f1441ceb87dd49a7b3eb834e9f3013b
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-13 09:39:42 +0300

    Initial Commit

---
+++ DragDropLists/DragDropListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropListsApp.swift
+//  DragDropLists
+//
+//  Created by Yunus Emre Taşdemir on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DragDropListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
